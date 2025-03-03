6f2c0a94cffa823f33c368482812156b5a0e6b49
Martin <martinbjerkaas@users.noreply.github.com>
2025-03-03 21:58:50 +0100

    Updating to new project

---
+++ expedito.d
@@ -0,0 +1,1 @@
+/home/martin/projects/rust/expedito/target/wasm32-unknown-emscripten/release/expedito.js: /home/martin/projects/rust/expedito/src/main.rs
--- walkthetalk.d
+++
@@ -1,1 +0,0 @@
-/home/martin/projects/rust/walkthetalk/target/wasm32-unknown-emscripten/release/walkthetalk.js: /home/martin/projects/rust/walkthetalk/src/main.rs
